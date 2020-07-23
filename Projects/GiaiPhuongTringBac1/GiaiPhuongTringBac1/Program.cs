c3d44af8a01edbf3defbf4923487430544868517
trinm31 <trinmgcd19833@fpt.edu.vn>
2020-07-23 18:34:01 +0700

    hello tri dday

---
+++ Projects/GiaiPhuongTringBac1/GiaiPhuongTringBac1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace GiaiPhuongTringBac1
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Chương trình giải phương trình bậc 1: ax+b=c");
+            Console.WriteLine("--------------------------------------");
+            Console.WriteLine("Nhap vao so a");
+            Double a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Nhap vao so b");
+            Double b = Convert.ToDouble(Console.ReadLine());
+            
+            if (a==0 && b==0)
+            {
+                Console.WriteLine("Phuong trinh vo so nghiem");
+            }
+            else if (a==0 && b!=0)
+            {
+                Console.WriteLine("Phuong trinh vo nghiem");
+            }
+            else
+            {
+                Console.WriteLine(tinhnghiem(a, b));
+            }
+            Console.ReadKey();
+        }
+
+        private static string tinhnghiem(double a, double b)
+        {
+            return $"Nghiem cua phuong trinh la:{(double)-b / a}";
+        }
+    }
+}
